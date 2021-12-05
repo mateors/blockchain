93cc3a5453c81a2f3838872adca27de3293bae8d
MOSTAIN BILLAH <bill.rassel@gmail.com>
2021-12-05 20:01:47 +0600

    authentication,getStats and other user info

--- taxtoken/main.js
+++ taxtoken/main.js
@@ -7,16 +7,25 @@ Moralis.start({ serverUrl, appId });
 /* Authentication code */
 async function login() {
   let user = Moralis.User.current();
+
   if (!user) {
-    user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
-      .then(function (user) {
-        console.log("logged in user:", user);
-        console.log(user.get("ethAddress"));
-      })
-      .catch(function (error) {
-        console(error);
-      });
+
+    // user = await Moralis.authenticate({ signingMessage: "Log in using TaxToken" })
+    //   .then(function (user) {
+    //     console.log("logged in user:", user);
+    //     console.log(user.get("ethAddress"));
+    //   })
+    //   .catch(function (error) {
+    //     console(error);
+    //   });
+
+    user = await Moralis.Web3.authenticate();
+
   }
+  // get stats on page load
+  console.log("logged in user:",user.get("ethAddress"));
+  getStats();
+
 }
 
 async function logOut() {
@@ -27,3 +36,22 @@ async function logOut() {
 
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
+
+document.getElementById("btn-get-stats").onclick = getStats;
+
+function getStats() {
+  const user = Moralis.User.current();
+  if (user) {
+    getUserTransactions(user);
+  }
+}
+
+async function getUserTransactions(user) {
+  // create query
+  const query = new Moralis.Query("EthTransactions");
+  query.equalTo("from_address", user.get("ethAddress"));
+
+  // run query
+  const results = await query.find();
+  console.log("user transactions:", results);
+}
